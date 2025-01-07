7a9c5bf398ab89c117dabd97e3414fdf0b54ef91
alerex.eth <alessandromaci96@gmail.com>
2025-01-08 00:32:25 +0100

    remove homepage from app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,17 @@
 import * as React from "react";
-import { useRouter } from "next/router";
-import Homepage from "../components/Home/Homepage";
 import Layout from "../components/Layout";
+import Payment from "../components/Payment/Payment";
 import SEO from "../components/Seo";
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <>
       <SEO
-        title="Woop Pay | Create Cryptocurrency Payment Requests"
-        description="Woop Pay is a web application that simplifies cryptocurrency payment requests. You can connect your wallet to create a payment request and share it. Woop Pay supports native tokens ETHER and MATIC, and popular ERC20 tokens such as DAI, USDC, TETHER, WETH, and WBTC. It also supports multiple networks within the Ethereum ecosystem: Mainnet, Goerli, Arbitrum, Optimism, and Polygon."
-        rrssImg="./RRSS.png"
+        title="Woop | Create Cryptocurrency Payment Requests Easily"
+        description="Woop is the easiest way to create, track, and receive cryptocurrency payment requests. Designed for both crypto natives and beginners."
       />
       <Layout>
-        <Homepage
-          onNavigateToPaymentRequest={() => router.push("/request")}
-          onNavigateToGenerateButton={() => router.push("/customize")}
-        />
+        <Payment />
       </Layout>
     </>
   );
