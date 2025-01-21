d5de1dd6bf523fdc9799445b817553a8c79c6f3d
alerex.eth <alessandromaci96@gmail.com>
2025-01-22 00:10:15 +0100

    clean-up

--- components/layout/LayoutReceive.tsx
+++ components/layout/LayoutReceive.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Header from "../common/Header";
-import styles from "../../pages/index.module.scss";
-import cx from "classnames";
 import Container from "@mui/material/Container";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -9,21 +7,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Header />
       <div
-        className={cx(
-          styles.baseContainer,
-          "min-h-screen flex justify-center items-center"
-        )}
+        className={
+          "min-h-screen w-full flex justify-center items-center bg-[#e6e7e9] text-black"
+        }
       >
-        {/* Background Section */}
-        <section
-          className={cx(
-            styles.containerBase,
-            "absolute top-0 left-0 w-full h-full z-0 opacity-50"
-          )}
-        ></section>
-
-        {/* Main Content */}
-        <Container className="relative z-10 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-xl p-2 mt-8 md:mt-16 bg-white rounded-lg">
+        <Container className="relative z-10 w-full max-w-screen-xl p-2 mt-8 md:mt-16 bg-white rounded-lg">
           {children}
         </Container>
       </div>
