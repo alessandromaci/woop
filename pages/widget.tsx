6d84de4449f64da6df25a90d0c71f9a1ff98f5e1
alerex.eth <alessandromaci96@gmail.com>
2025-03-07 13:56:45 +0100

    update the widget layout

--- pages/widget.tsx
+++ pages/widget.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import SEO from "../components/common/Seo";
 import WidgetLayout from "../components/layout/WidgetLayout";
-import Payment from "../components/Payment/Payment";
+import RequestAmount from "../components/Payment/1_RequestAmount";
+import SelectReceiptMethod from "../components/Payment/2_SelectReceiptMethod ";
+//import Payment from "../components/Payment/Payment";
 import { tokensDetails } from "../utils/constants";
 
 export default function WidgetPage() {
@@ -12,6 +14,11 @@ export default function WidgetPage() {
   const [currencies, setCurrencies] = React.useState(
     tokensDetails.map((token) => token.label)
   );
+  const [chainId, setChainId] = React.useState<string>("");
+  const [currentStep, setCurrentStep] = React.useState(1);
+  const [selectedAmount, setSelectedAmount] = React.useState("");
+  const [selectedToken, setSelectedToken] = React.useState();
+  const [selectedDescription, setSelectedDescription] = React.useState("");
 
   return (
     <>
@@ -31,12 +38,36 @@ export default function WidgetPage() {
             theme === "dark" ? "bg-black" : "bg-white"
           }`}
         >
-          <Payment
-            logo={logo}
-            theme={theme}
-            buttonColor={buttonColor}
-            currencies={currencies}
-          />
+          {currentStep === 1 && (
+            <RequestAmount
+              onContinue={(amount: any, token: any, description: string) => {
+                setSelectedAmount(amount);
+                setSelectedToken(token);
+                setSelectedDescription(description);
+                setCurrentStep(2);
+              }}
+              theme={theme}
+              logo={logo}
+              buttonColor={buttonColor}
+              currencies={currencies}
+              chainId={chainId}
+              setChainId={setChainId}
+            />
+          )}
+          {currentStep === 2 && (
+            <SelectReceiptMethod
+              onBack={() => setCurrentStep(1)}
+              selectedAmount={selectedAmount}
+              selectedToken={selectedToken}
+              selectedDescription={selectedDescription}
+              theme={theme}
+              logo={logo}
+              buttonColor={buttonColor}
+              currencies={currencies}
+              chainId={chainId}
+              setChainId={setChainId}
+            />
+          )}
         </div>
       </WidgetLayout>
     </>
