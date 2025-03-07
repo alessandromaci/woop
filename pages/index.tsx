c2aa64b523d30fb33122cf7b3ee28b22f27f7d87
alerex.eth <alessandromaci96@gmail.com>
2025-03-07 08:57:31 +0100

    update payment flow in multi-steps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Layout from "../components/layout/LayoutReceive";
-import Payment from "../components/Payment/Payment";
-import InstantOffRampEventsSDK from "../components/Transak";
+//import Payment from "../components/Payment/Payment";
+import RequestAmount from "../components/Payment/1_RequestAmount";
+import SelectReceiptMethod from "../components/Payment/2_SelectReceiptMethod ";
 import SEO from "../components/common/Seo";
 import { tokensDetails } from "../utils/constants";
 
@@ -15,6 +16,10 @@ export default function Home() {
   const [currencies] = React.useState(
     tokensDetails.map((token) => token.label)
   );
+  const [currentStep, setCurrentStep] = React.useState(1);
+  const [selectedAmount, setSelectedAmount] = React.useState("");
+  const [selectedToken, setSelectedToken] = React.useState();
+  const [selectedDescription, setSelectedDescription] = React.useState("");
 
   return (
     <>
@@ -23,7 +28,7 @@ export default function Home() {
         description="Woop is the easiest way to create, track, and receive cryptocurrency payment requests. Designed for both crypto natives and beginners."
         rrssImg="./RRSS.jpg"
       />
-      <Layout>
+      {/* <Layout>
         <Payment
           logo={logo}
           theme={theme}
@@ -36,6 +41,40 @@ export default function Home() {
         {chainId === "Bank" && (
           <InstantOffRampEventsSDK
             onWalletAddressReceived={setRecipientAddressTransak}
+          />
+        )}
+      </Layout> */}
+      <Layout>
+        {currentStep === 1 && (
+          <RequestAmount
+            onContinue={(amount: any, token: any, description: string) => {
+              setSelectedAmount(amount);
+              setSelectedToken(token);
+              setSelectedDescription(description);
+              setCurrentStep(2);
+            }}
+            theme={theme}
+            logo={logo}
+            buttonColor={buttonColor}
+            currencies={currencies}
+            recipientAddressTransak={recipientAddressTransak}
+            chainId={chainId}
+            setChainId={setChainId}
+          />
+        )}
+        {currentStep === 2 && (
+          <SelectReceiptMethod
+            onBack={() => setCurrentStep(1)}
+            selectedAmount={selectedAmount}
+            selectedToken={selectedToken}
+            selectedDescription={selectedDescription}
+            theme={theme}
+            logo={logo}
+            buttonColor={buttonColor}
+            currencies={currencies}
+            recipientAddressTransak={recipientAddressTransak}
+            chainId={chainId}
+            setChainId={setChainId}
           />
         )}
       </Layout>
