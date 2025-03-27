1f2cb09f964dea1096d596e9858332d5a55ee66f
alerex.eth <alessandromaci96@gmail.com>
2025-03-27 09:58:17 +0100

    adding wip to missing modules

--- components/common/Navigation.tsx
+++ components/common/Navigation.tsx
@@ -18,26 +18,24 @@ export default function Navigation({ activeTab }: NavigationProps) {
       >
         Receive
       </Link>
-      <Link
-        href="/invest"
-        className={`flex justify-center items-center font-sans text-sm leading-snug font-medium w-1/3 h-10 rounded-lg transition-all ${
-          activeTab === "invest"
-            ? "bg-[#4285F4] text-white"
-            : "bg-[#F5F5F5] text-[#666666] hover:bg-[#EBEBEB]"
-        }`}
-      >
-        Invest
-      </Link>
-      <Link
-        href="/nfts"
-        className={`flex justify-center items-center font-sans text-sm leading-snug font-medium w-1/3 h-10 rounded-lg transition-all ${
-          activeTab === "nfts"
-            ? "bg-[#4285F4] text-white"
-            : "bg-[#F5F5F5] text-[#666666] hover:bg-[#EBEBEB]"
-        }`}
-      >
-        NFTs
-      </Link>
+      {/* Invest - Coming Soon */}
+      <div className="relative w-1/3">
+        <div className="flex justify-center items-center font-sans text-sm leading-snug font-medium h-10 rounded-lg bg-[#F5F5F5] text-[#666666] opacity-75">
+          Invest
+        </div>
+        <div className="absolute right-0 top-0 -translate-y-1/2 px-2 py-1 bg-blue-400 text-white text-xs rounded-full font-sans">
+          WIP
+        </div>
+      </div>
+      {/* NFTs - Coming Soon */}
+      <div className="relative w-1/3">
+        <div className="flex justify-center items-center font-sans text-sm leading-snug font-medium h-10 rounded-lg bg-[#F5F5F5] text-[#666666] opacity-75">
+          NFTs
+        </div>
+        <div className="absolute right-0 top-0 -translate-y-1/2 px-2 py-1 bg-blue-400 text-white text-xs rounded-full font-sans">
+          WIP
+        </div>
+      </div>
     </div>
   );
 }
